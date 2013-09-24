98db90982bedd883f31b48a14ec63999f1685021
Sithu Aung <a.sithu@gmail.com>
2013-09-24 10:12:58 -0700

    Update run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,2 @@
-nohup java -jar target/dropwizard-helloworld-0.0.1-SNAPSHOT.jar server config/dev_config.yml > /tmp/console.log 2>&1&
-echo $! > /tmp/app.pid
+java -jar target/dropwizard-helloworld-0.0.1-SNAPSHOT.jar server config/dev_config.yml
  
