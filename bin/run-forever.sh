ed1af28b38cdfd926d8f080474ba028fac8a0da1
sithu <sithumail@gmail.com>
2013-09-26 16:42:38 -0700

    fixed storing worng PID of the app

--- bin/run-forever.sh
+++ bin/run-forever.sh
@@ -1,5 +1,3 @@
 echo "run-forever.sh:: Start running the app..."
 nohup ./bin/run.sh 0<&- &> /tmp/app.log &
-# save the PID in a temp file
-echo $! > /tmp/app.pid
 echo "run-forever.sh:: Successfully started the app"
--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,6 @@
-echo "run.sh:: running the app"
+echo "run.sh::Running the app"
 java -jar target/dropwizard-helloworld-0.0.1-SNAPSHOT.jar server config/dev_config.yml
- 
+# save the PID in a temp file
+echo $! > /tmp/app.pid
+pid=$!
+echo "run.sh::The app PID is $pid"   
