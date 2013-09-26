329190b737c08b8a40546878a9a9626cd614db01
sithu <sithumail@gmail.com>
2013-09-26 16:50:34 -0700

    nohup must run java command directly to capture the right PID

--- bin/run-forever.sh
+++ bin/run-forever.sh
@@ -1,3 +1,8 @@
 echo "run-forever.sh:: Start running the app..."
-nohup ./bin/run.sh 0<&- &> /tmp/app.log &
-echo "run-forever.sh:: Successfully started the app"
+
+nohup java -jar target/dropwizard-helloworld-0.0.1-SNAPSHOT.jar server config/dev_config.yml 0<&- &> /tmp/app.log &
+
+# save the PID in a temp file
+echo $! > /tmp/app.pid
+pid=$!
+echo "run-forever.sh:: Successfully started the app (pid:$pid)"
