e22113c3c833289f1080c4a499c3887adbff6a37
sithu <sithumail@gmail.com>
2013-09-26 17:37:01 -0700

    fixed run-forever issue

--- bin/run-forever.sh
+++ bin/run-forever.sh
@@ -1,6 +1,6 @@
 echo "run-forever.sh:: Start running the app..."
 
-nohup .bin/run.sh 0<&- &> /tmp/app.log &
+nohup ./bin/run.sh 0<&- &> /tmp/app.log &
 
 # Grep the pid of Java process and save the PID in a temp file
 # Specifically look for dropwizard jar `ps -elf | awk '/dropwizard-helloworld.*.jar/ {print $4}' | head -1`
