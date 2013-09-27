fee02a942bcd1ecb2404d7ba8f18321a0251f4fe
sithu <sithumail@gmail.com>
2013-09-26 17:15:41 -0700

    testing pid issue

--- bin/run-forever.sh
+++ bin/run-forever.sh
@@ -5,4 +5,5 @@ nohup java -jar target/dropwizard-helloworld-0.0.1-SNAPSHOT.jar server config/de
 # save the PID in a temp file
 echo $! > /tmp/app.pid
 pid=$!
-echo "run-forever.sh:: Successfully started the app (pid:$pid)"
+pidd=$$
+echo "run-forever.sh:: Successfully started the app (pid:$pid)-$pidd"
